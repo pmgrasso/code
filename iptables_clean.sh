e43c80b0f083dca1df4f4dda4de67be0f3f08f80
PG <pmgrasso@gmail.com>
2022-08-20 17:48:28 +0200

    test github

---
+++ iptables_clean.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+# 20210604
+
+echo "ELIMINANDO REGLAS IPTABLES"
+
+/sbin/iptables -P INPUT ACCEPT
+/sbin/iptables -P FORWARD ACCEPT
+/sbin/iptables -P OUTPUT ACCEPT
+/sbin/iptables -t nat -F
+/sbin/iptables -t mangle -F
+/sbin/iptables -F
+/sbin/iptables -X
+
+echo "OK"
+
+
+
+exit
