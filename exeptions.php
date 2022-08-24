7e747e675c73336b1ce2fcc4b501e3b0ff8ef1c3
PG <pmgrasso@gmail.com>
2022-08-24 15:45:27 +0200

    Php Exceptions

---
+++ exeptions.php
@@ -0,0 +1,34 @@
+<?php
+
+
+class theType2
+{
+    public function whatType (int $input , string $type) : bool {
+        if(gettype($input) == $type && $input >= 0 ){  
+        return TRUE;
+        } else {
+            throw new Exception("Value must be ".$type);
+        } 
+        //return FALSE;        
+    }
+}
+
+//$input = 5;
+$input = -1;
+$type = 'integer';
+
+
+try {
+    $obj = new theType2();
+    $obj->whatType($input,$type);
+    echo 'OK';
+} catch(Exception $e) {
+    echo $e->getMessage();
+}
+
+unset($obj);
+//var_dump($obj);
+
+//echo "<br><br>done";
+
+?>
